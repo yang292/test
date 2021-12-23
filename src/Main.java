f5ea708da15a84b38170fa2ed266cf57847899ae
yangjun <yangjun@oxhainan.org>
2021-12-23 16:26:43 +0800

    123213

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("今天的日期为:"+today);
+        LocalDate nextWeek = today.plus(1, ChronoUnit.WEEKS);
+        System.out.println("一周后的日期为:"+nextWeek);
+
+        Clock clock = Clock.systemUTC();
+        System.out.println("Clock : " + clock.millis());
+
+        // Returns time based on system clock zone
+        Clock defaultClock = Clock.systemDefaultZone();
+        System.out.println("Clock : " + defaultClock.millis());
+    }
+}
